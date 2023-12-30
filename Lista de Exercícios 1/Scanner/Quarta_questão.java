a01bc000fe2c77dfc924232c2a2b6642ec7c1795
Marcos Gabriel <152616069+marcosgabrielmg@users.noreply.github.com>
2023-12-30 16:03:44 -0300

    quant. de dígitos

--- Lista de Exercícios 1/Scanner/Quarta_questão.java
+++ Lista de Exercícios 1/Scanner/Quarta_questão.java
@@ -17,14 +17,22 @@ public class Quarta_questão {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int i=0;
         Scanner leia = new Scanner (System.in);
         System.out.println("Digite um valor inteiro maior ou igual a zero: ");
-        int vi = leia.nextInt();
-            while(vi<0){
-                System.out.println("Valor inaceitável. Digite outro valor inteiro maior ou igual a zero: ");
-                vi = leia.nextInt();
+        long vi = leia.nextLong();
+            while(vi<=0){
+                System.out.println("Valor inaceitavel. Digite outro valor inteiro maior ou igual a zero: ");
+                vi = leia.nextLong();
             }
         
+        while(vi>0){
+            vi = vi/10;
+            i++;
+        }
+        
+        System.out.println("O valor inserido tem " + i + " digito(s).");
     }
+}
     
 }
